c67c960833e2e889fc252d4b6280e491b6e86d96
TlhalefoSisa1 <sisatlahlefo@gmail>
2020-05-20 15:40:04 +0200

    i added new spec on password is ok

--- spec/password_is_ok_spec.js
+++ spec/password_is_ok_spec.js
@@ -1,5 +1,11 @@
-let passwordIsOk = require ('../src/password_checker'); 
+let password = require ('../src/password_checker'); 
 
 describe ("function checks the password_Is_Ok", function(){
-    it
+    it("returns false if password does not match", function(){
+        expect(password.passwordIsOk("sdHH12*#")).toBe(false)
+    })
+
+    it("returns true if password match", function(){
+        expect(password.passwordIsOk("9999dHH12*#")).toBe(true)
+    })
 })
