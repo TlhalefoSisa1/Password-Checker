bd7cb342f7bc18346f8ec343bf6d5acb2ac988dc
TlhalefoSisa1 <sisatlahlefo@gmail>
2020-05-20 15:51:37 +0200

    I fix the funtion masseges that i copied and pasted

--- spec/password_is_valid_spec.js
+++ spec/password_is_valid_spec.js
@@ -1,26 +1,26 @@
 let passwordIsValid = require ('../src/password_checker');
 var password = "Um#xh8dgr"
 describe("function checks the password_Is_Valid", function(){
-   it("should provide outcome for password checker", function() {
+   it("password should exist", function() {
        expect(password.passwordIsValid).not.toBeNull("")
       });
     
-   it("should provide outcome for password checker", function() {
+   it("password should be longer than than 8 characters", function() {
         expect(password.length).toBeGreaterThan(8)
       });
       
-   it("should provide outcome for password checker", function() {
+   it("password should have at least one lowercase lette", function() {
        expect(password).toMatch(/[a-z]/);
        }); 
 
-   it("should provide outcome for password checker", function() {
+   it("password should have at least one upperrcase letter", function() {
         expect(password).toMatch(/[A-Z]/);
      }); 
            
-   it("should provide outcome for password checker", function() {
+   it("password should at least have one digit", function() {
        expect(password).toMatch(/[0-9]/);
    })
-   it("should provide outcome for password checker", function() {
+   it("password should have at least one special character", function() {
            expect(password).toMatch(/[{!@#$%^&*.\*}]/);  
    });        
 })
